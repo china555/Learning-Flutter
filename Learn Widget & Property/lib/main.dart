1d0c1c1c4fe09c9d646c3a17f4e6368d741aa2c0
Chinapanda <kittikornchina2014@gmail.com>
2021-02-05 19:01:57 +0700

    learn Widget & property

---
+++ Learn Widget & Property/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello china'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+        ),
+      ),
+    ));
