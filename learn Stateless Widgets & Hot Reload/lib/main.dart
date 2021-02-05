431d58a017caff137862ee035189a9f1db6b7ea4
Chinapanda <kittikornchina2014@gmail.com>
2021-02-05 18:53:24 +0700

    Stateless Widgets & Hot Reload

---
+++ learn Stateless Widgets & Hot Reload/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        backgroundColor: Colors.pink[200],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Hello china',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'HachiMaruPop',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.pink[500],
+      ),
+    );
+  }
+}
