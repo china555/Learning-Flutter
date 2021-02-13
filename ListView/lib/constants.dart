784f19608aef42dab82f1cba6420ba27dd4ad322
Chinapanda <kittikornchina2014@gmail.com>
2021-02-14 01:29:16 +0700

    ListView HomeWork04

---
+++ ListView/lib/constants.dart
@@ -0,0 +1,53 @@
+const FOOD_DATA = [
+  {"name": "PUU AAD", "brand": "NONGNAM", "price": 10, "image": "1.png"},
+  {
+    "name": "KUNG MANG KORN",
+    "brand": "NONGNAM",
+    "price": 50,
+    "image": "2.jpg"
+  },
+  {"name": "SAA RAAI", "brand": "NONGNAM", "price": 20, "image": "3.png"},
+  {
+    "name": "SALMON NOT BURN~~~",
+    "brand": "NONGNAM",
+    "price": 30,
+    "image": "4.png"
+  },
+  {
+    "name": "MAENG KRA PRUNE",
+    "brand": "NONGNAM",
+    "price": 20,
+    "image": "5.png"
+  },
+  {
+    "name": "FISH SLIDE GRILL CEE EEL",
+    "brand": "NONGNAM",
+    "price": 50,
+    "image": "6.png"
+  },
+  {
+    "name": "BANGKOK ROLL",
+    "brand": "NONGNAM",
+    "price": 200,
+    "image": "7.jpg"
+  },
+  {
+    "name": "SWEET BALLS",
+    "brand": "NONGNAM",
+    "price": 10000,
+    "image": "8.jpg"
+  },
+  {
+    "name": "BANGKOK SALMON GONE",
+    "brand": "NONGNAM",
+    "price": 200,
+    "image": "9.jpg"
+  },
+  {"name": "KUNG BALLS", "brand": "NONGNAM", "price": 30, "image": "10.jpg"},
+  {
+    "name": "SALMON BINGSUU",
+    "brand": "NONGNAM",
+    "price": 150000,
+    "image": "11.jpg"
+  },
+];
