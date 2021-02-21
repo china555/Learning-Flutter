66b37f6c45f944a3a2da145c2832a66ab9fac459
Chinapanda <kittikornchina2014@gmail.com>
2021-02-21 14:54:40 +0700

    example UI for project

--- orientation/lib/main.dart
+++ orientation/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.landscapeRight,
     ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: appTitle,
       home: OrientationList(
         title: appTitle,
@@ -31,67 +32,40 @@ class OrientationList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Text(
-                'Hello',
-                style: TextStyle(
-                  fontSize: 100,
-                  color: Colors.amber,
-                ),
+      backgroundColor: Colors.indigo[900],
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                top: (MediaQuery.of(context).size.height) / 2 - 60),
+            child: Text(
+              'Hello',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30), color: Colors.amber),
-              child: Text('60'),
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(top: (MediaQuery.of(context).size.height / 6)),
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.amber[700],
+                ),
+                child: Text('60',style: TextStyle(fontSize: 20),),
+              ),
             ),
-          ],
-        ),
-        // child: Container(
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-
-        //     children: [
-        //       Text(
-        //         'Hello',
-        //         style: TextStyle(
-        //           fontSize: 100,
-        //           color: Colors.amber,
-        //         ),
-        //       ),
-        //       Text('60')
-        //     ],
-        //   ),
-        // ),
+          ),
+        ],
       ),
     );
   }
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(title: Text(title)),
-  //     body: OrientationBuilder(
-  //       builder: (context, orientation) {
-  //         return GridView.count(
-  //           // Create a grid with 2 columns in portrait mode, or 3 columns in
-  //           // landscape mode.
-  //           crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-  //           // Generate 100 widgets that display their index in the List.
-  //           children: List.generate(100, (index) {
-  //             return Center(
-  //               child: Text(
-  //                 'Item $index',
-  //                 style: Theme.of(context).textTheme.headline1,
-  //               ),
-  //             );
-  //           }),
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
 }
